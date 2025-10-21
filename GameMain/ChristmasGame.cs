baebc909bbef493612a4c41d6f9ae4b7bdc66a83
Casper Huikeshoven <c.huikeshoven@students.uu.nl>
2025-10-21 17:53:10 +0200

    #0 Add StartState

---
+++ GameMain/ChristmasGame.cs
@@ -0,0 +1,34 @@
+﻿using Engine;
+using GameMain.GameStates;
+using System;
+
+namespace GameMain
+{
+    public class ChristmasGame : ExtendedGame
+    {
+        public const string Key_StartState = "startState";
+
+        [STAThread]
+        static void Main()
+        {
+            ChristmasGame game = new ChristmasGame();
+            game.Run();
+        }
+
+        public ChristmasGame()
+        {
+            GameStateManager.AddGameState(new StartState(GameStateManager), Key_StartState);
+            GameStateManager.SwitchState(Key_StartState);
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+    }
+}
---
+++ GameMain/GameStates/StartState.cs
@@ -0,0 +1,11 @@
+﻿using Engine;
+
+namespace GameMain.GameStates
+{
+    internal class StartState : GameState
+    {
+        public StartState(GameStateManager gameStateManager) : base(gameStateManager)
+        {
+        }
+    }
+}
